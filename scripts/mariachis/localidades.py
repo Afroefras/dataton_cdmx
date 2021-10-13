4e6b7301baa1f8f23f939e757ab3c7c1ba56cdf1
EF <efraisma.ef7@gmail.com>
2021-10-13 12:24:06 -0500

    Clustering de alcaldías según disttribución de clustering de ILE

--- scripts/mariachis/localidades.py
+++ scripts/mariachis/localidades.py
@@ -37,30 +37,42 @@ class GeoLoc(BaseClass):
     def get_data(self, decode_to: str='utf-8', replace_dict: Dict={'México':'Estado de México','Distrito Federal':'Ciudad de México'}) -> DataFrame:
         # Obtiene la información del request
         req_data = get_req(self.zip_url).content
+
         # Optimizando memoria, obtiene los datos del zip
         zipfile = ZipFile(BytesIO(req_data))
+
         # Lista vacía para agregar cada renglón del archivo de interés
         data = []
         # Para cada renglón del archivo txt con la información de interés
         for line in zipfile.open(f'{self.country}.txt').readlines():
             # Añadirlo a la lista ya decodificado
             data.append(line.decode(decode_to))
+
         # Estructurarlo en un DataFrame para manipulación posterior
         df = DataFrame(map(lambda x: x.replace('\n','').split('\t'),data), columns=self.cols)
         self.cool_print(f'Códigos postales de {self.country} importados desde {self.zip_url}')
+
         df = df.replace(replace_dict)
+        
         # Exporta los resultados en formato csv
         self.export_csv(df, index=False, sep='\t', encoding='utf-16')
         return df
 
-    def wrangling_geo(self, df: DataFrame, group_by_cols: list=['state_name','province_name'], to_keep: list=['country_code','state_name','state_code','province_name','province_code','lat','lon']) -> DataFrame:
-        # Construye el polígono de geolocalización
-        df = df[to_keep].copy()
+    def wrangling_geo(self, data: DataFrame, filter_places: Dict={'state_name':['Ciudad de México','Estado de México']}, group_by_cols: list=['state_name','province_name']) -> DataFrame:
+        # Filtra los lugares indicados en el parámetro "filter_places"
+        df = DataFrame()
+        for level, places in filter_places.items():
+            sub_df = data[data[level].isin(places)].copy()
+            df = df.append(sub_df, ignore_index=True)
+
         df['group'] = df[group_by_cols].apply(', '.join, axis=1)
+
         # Construye el polígono de geolocalización
         df = self.geo_polygon(df, group_by='group')
+
         # Crea variables de geolocalización importantes
         df = self.geo_metrics(df)
+        
         # Exporta los resultados en formato csv
         self.export_csv(df, name_suffix='geoloc', index=False)
         return df
